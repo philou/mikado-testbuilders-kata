072bfd82c520850b84a2d562da4b6fcef2dd0afe
Philippe Bourgau <philippe.bourgau@murex.com>
2020-06-25 16:46:50 +0200

    Working...

--- src/test/cpp/purchase/InvoiceTest.cpp
+++ src/test/cpp/purchase/InvoiceTest.cpp
@@ -1,15 +1,28 @@
 #include "gtest/gtest.h"
 #include "purchase/Invoice.h"
 #include "finance/TaxRule.h"
+#include "domain/book/Book.h"
+#include "domain/book/Novel.h"
+#include "purchase/PurchasedBook.h"
 
 using namespace std;
 
-TEST(InvoiceTest, total_amount_is_converted_to_invoice_currency_at_current_rate)
+TEST(InvoiceTest, total_amount_is_converted_to_currency_of_invoice_country_at_current_rate)
 {
-   purchase::Invoice invoice;
+   auto grapesOfWrath = std::make_shared<domain::book::Novel>(
+      "Grapes of Wrath", 
+      6.99, 
+      domain::book::Author("John Steinbeck", domain::country::Country("USA", domain::country::Currency::US_DOLLAR, domain::country::Language::ENGLISH)), 
+      domain::country::Language::ENGLISH, 
+      std::vector<domain::book::Genre>());
+
+   purchase::Invoice invoice("John Doe", domain::country::Country("France", domain::country::Currency::EURO, domain::country::Language::FRENCH));
+   invoice.addPurchasedBook(std::make_shared<purchase::PurchasedBook>(grapesOfWrath, 1));
    // create an invoice with a different currency
 
    const auto totalAmount = invoice.computeTotalAmount();
 
-   EXPECT_EQ(book.Price * finance::getApplicableRate(country, book));
+   EXPECT_EQ(
+      grapesOfWrath->getPrice() * finance::getApplicableRate(invoice.getCountry(), *grapesOfWrath), 
+      totalAmount);
 }
