d64408919a1a76508d383b3eb8cb0d9194a65eec
Philippe Bourgau <philippe.bourgau@murex.com>
2020-06-25 17:23:16 +0200

    Working...

--- src/test/cpp/purchase/InvoiceTest.cpp
+++ src/test/cpp/purchase/InvoiceTest.cpp
@@ -10,21 +10,6 @@ using namespace domain::book;
 using namespace domain::country;
 using namespace purchase;
 
-TEST(InvoiceTest, total_amount_is_converted_to_currency_of_invoice_country_at_current_rate)
+TEST(InvoiceTest, DISABLED_total_amount_is_converted_to_currency_of_invoice_country_at_current_rate)
 {
-   auto grapesOfWrath = std::make_shared<Novel>(
-      "Grapes of Wrath", 
-      6.99, 
-      Author("John Steinbeck", Country("USA", US_DOLLAR, ENGLISH)), 
-      ENGLISH, 
-      std::vector<Genre>());
-
-   Invoice invoice("John Doe", Country("France", EURO, FRENCH));
-   invoice.addPurchasedBook(std::make_shared<PurchasedBook>(grapesOfWrath, 1));
-
-   const auto totalAmount = invoice.computeTotalAmount();
-
-   EXPECT_EQ(
-      grapesOfWrath->getPrice() * finance::getApplicableRate(invoice.getCountry(), *grapesOfWrath), 
-      totalAmount);
 }
--- src/test/cpp/report/ReportGeneratorTest.cpp
+++ src/test/cpp/report/ReportGeneratorTest.cpp
@@ -13,26 +13,6 @@ using namespace domain::country;
 using namespace purchase;
 using namespace report;
 
-TEST(ReportGeneratorTest, converts_invoice_amounts_to_USD_before_summing_them)
+TEST(ReportGeneratorTest, DISABLED_converts_invoice_amounts_to_USD_before_summing_them)
 {
-   auto grapesOfWrath = std::make_shared<Novel>(
-      "Grapes of Wrath",
-      6.99,
-      Author("John Steinbeck", Country("USA", US_DOLLAR, ENGLISH)),
-      ENGLISH,
-      std::vector<Genre>());
-
-   auto invoice = make_shared<Invoice>("John Doe", Country("France", EURO, FRENCH));
-   invoice->addPurchasedBook(std::make_shared<PurchasedBook>(grapesOfWrath, 1));
-
-   auto repository = make_shared<storage::InMemoryRepository>();
-   repository->addInvoice(invoice);
-
-   main_repository::override(repository);
-
-   ReportGenerator reportGenerator;
-
-   const auto totalAmount = reportGenerator.getTotalAmount();
-
-   EXPECT_EQ(finance::toUSD(invoice->computeTotalAmount(), invoice->getCountry().getCurrency()), totalAmount);
 }
