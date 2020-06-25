2ef0afdb66b72cf93adf92d5c84d6a3aafc7c675
Philippe Bourgau <philippe.bourgau@murex.com>
2020-06-25 16:36:12 +0200

    Working...

--- src/test/cpp/purchase/InvoiceTest.cpp
+++ src/test/cpp/purchase/InvoiceTest.cpp
@@ -3,7 +3,12 @@
 
 using namespace std;
 
-TEST(InvoiceTest, DISABLED_empty_test)
+TEST(InvoiceTest, total_amount_is_converted_to_invoice_currency_at_current_rate)
 {
-   EXPECT_TRUE(false);
+   purchase::Invoice invoice;
+   // create an invoice with a different currency
+
+   const auto totalAmount = invoice.computeTotalAmount();
+
+   // assert that it is as expected
 }
