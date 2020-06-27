742635337f0d34dcd4f081b461b2944aa5303749
Philippe Bourgau <philippe.bourgau@murex.com>
2020-06-27 07:00:32 +0200

    Commented out the fixes

--- src/main/cpp/lib/Invoice.cpp
+++ src/main/cpp/lib/Invoice.cpp
@@ -40,7 +40,8 @@ double purchase::Invoice::computeTotalAmount() const
 	double sum = 0.0;
 	for (const auto purchasedBook : purchasedBooks_)
 	{
-      double totalPrice = purchasedBook->getTotalPrice() * finance::getApplicableRate(country_, *purchasedBook->getBook());
+      double totalPrice = purchasedBook->getTotalPrice();
+      //double totalPrice = purchasedBook->getTotalPrice() * finance::getApplicableRate(country_, *purchasedBook->getBook());
       sum += totalPrice;
 	}
 	return sum;
