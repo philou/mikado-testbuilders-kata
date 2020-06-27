ae3ea79063e71427b8d87b2e1c37ca2a29b11640
Philippe Bourgau <philippe.bourgau@murex.com>
2020-06-27 08:35:28 +0200

    Add France test country

---
+++ src/test/cpp/domain/country/TestCountries.h
@@ -0,0 +1,16 @@
+#ifndef TESTBUILDERS_WORKSHOP_TESTS_DOMAIN_COUNTRY_TEST_COUNTRIES_H_INCLUDED
+#define TESTBUILDERS_WORKSHOP_TESTS_DOMAIN_COUNTRY_TEST_COUNTRIES_H_INCLUDED
+
+#include "domain/country/Country.h"
+
+namespace domain
+{
+   namespace country
+   {
+      const Country France("France", EURO, FRENCH);
+   }
+}
+
+
+
+#endif //TESTBUILDERS_WORKSHOP_TESTS_DOMAIN_COUNTRY_TEST_COUNTRIES_H_INCLUDED
--- src/test/cpp/purchase/InvoiceTest.cpp
+++ src/test/cpp/purchase/InvoiceTest.cpp
@@ -10,6 +10,17 @@ using namespace domain::book;
 using namespace domain::country;
 using namespace purchase;
 
-TEST(InvoiceTest, DISABLED_total_amount_is_converted_to_currency_of_invoice_country_at_current_rate)
+TEST(InvoiceTest, total_amount_is_converted_to_currency_of_invoice_country_at_current_rate)
 {
+   //auto novel = aNovel()
+   //   .costing(7.99)
+   //   .build();
+
+   //auto invoice = anInvoice()
+   //   .from(France)
+   //   .build();
+
+   //EXPECT_EQ(
+   //   book.getPrice() * finance::getApplicableRate(invoiceCountry, book),
+   //   invoice.computeTotalAmount());
 }
