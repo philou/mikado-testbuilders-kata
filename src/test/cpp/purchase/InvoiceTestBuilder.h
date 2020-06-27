247673e07a46ec9dc358f818ee0b16bcabe4f726
Philippe Bourgau <philippe.bourgau@murex.com>
2020-06-27 08:50:12 +0200

    Add .costing() to InvoiceTestBuilder

--- src/test/cpp/purchase/InvoiceTestBuilder.h
+++ src/test/cpp/purchase/InvoiceTestBuilder.h
@@ -1,5 +1,6 @@
 #ifndef TESTBUILDERS_WORKSHOP_TESTS_PURCHASE_INVOICE_TEST_BUILDER_H_INCLUDED
 #define TESTBUILDERS_WORKSHOP_TESTS_PURCHASE_INVOICE_TEST_BUILDER_H_INCLUDED
+
 #include "domain/country/TestCountries.h"
 
 namespace purchase
@@ -8,11 +9,20 @@ namespace purchase
    {
    public:
 
+      InvoiceTestBuilder& from(domain::country::Country country)
+      {
+         this->country_ = std::make_shared<domain::country::Country>(country);
+         return *this;
+      }
+
       Invoice build()
       {
-         return Invoice("John Doe", domain::country::test_countries::France);
+         return Invoice("John Doe", domain::country::Country(*country_));
       }
-      
+
+   private:
+      std::shared_ptr<domain::country::Country> country_ = 
+         std::make_shared<domain::country::Country>(domain::country::test_countries::France);
    };
 
    InvoiceTestBuilder anInvoice()
