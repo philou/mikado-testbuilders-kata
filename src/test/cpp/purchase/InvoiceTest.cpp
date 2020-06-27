ce0c2150d4adec972d33a1e61cb5d7c15c47f024
Philippe Bourgau <philippe.bourgau@murex.com>
2020-06-27 07:15:29 +0200

    instantiate USA country for InvoiceTest

--- src/test/cpp/purchase/InvoiceTest.cpp
+++ src/test/cpp/purchase/InvoiceTest.cpp
@@ -13,8 +13,12 @@ using namespace purchase;
 TEST(InvoiceTest, total_amount_is_converted_to_currency_of_invoice_country_at_current_rate)
 {
    Country france("France", Currency::EURO, Language::FRENCH);
-
    Invoice invoice("John Doe", france);
+
+   Country usa("USA", Currency::US_DOLLAR, Language::ENGLISH);
+   //Author steinbeck("John Steinbeck", USA);
+
+   //Novel grapesOfWrath("Grapes of Wrath", 7.99, , Language::ENGLISH, vector<Genre>());
 
 //   EXPECT_EQ(bookPrice * rate, invoice.computeTotalAmount);
 }
