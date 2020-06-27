32b0c8d673c91af00aece82d517029458439d5e4
Philippe Bourgau <philippe.bourgau@murex.com>
2020-06-27 07:53:47 +0200

    Instantiate a country for author

--- src/test/cpp/report/ReportGeneratorTest.cpp
+++ src/test/cpp/report/ReportGeneratorTest.cpp
@@ -27,7 +27,15 @@ TEST_F(ReportGeneratorTest, converts_invoice_amounts_to_USD_before_summing_them)
    main_repository::override(make_shared<InMemoryRepository>());
 
    Country france("France", Currency::EURO, Language::FRENCH);
+   Country usa("USA", Currency::US_DOLLAR, Language::ENGLISH);
+
+   //Author steinbeck("John Steinbeck", )
+
+   //auto grapesOfWrath = make_shared<Novel>("Grapes of Wrath", 3.99, , Language::ENGLISH, vector<Genre>());
+
    Invoice invoice("John Doe", france);
+   //invoice.addPurchasedBook(make_shared<PurchasedBook>(), 1)
+
    ReportGenerator reportGenerator;
    EXPECT_EQ(
       finance::toUSD(invoice.computeTotalAmount(), invoice.getCountry().getCurrency()),
