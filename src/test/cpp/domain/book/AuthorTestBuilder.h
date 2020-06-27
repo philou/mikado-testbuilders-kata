2eb940a31dc3fc3c47fc47837831b89dd0ac157f
Philippe Bourgau <philippe.bourgau@murex.com>
2020-06-27 08:58:24 +0200

    Start Author test builder

---
+++ src/test/cpp/domain/book/AuthorTestBuilder.h
@@ -0,0 +1,27 @@
+#ifndef TESTBUILDERS_WORKSHOP_TESTS_DOMAIN_BOOK_AUTHOR_TEST_BUILDER_H_INCLUDED
+#define TESTBUILDERS_WORKSHOP_TESTS_DOMAIN_BOOK_AUTHOR_TEST_BUILDER_H_INCLUDED
+
+#include "domain/country/TestCountries.h"
+#include "domain/book/Author.h"
+
+namespace domain
+{
+   namespace book
+   {
+ /*     class AuthorTestBuilder
+      {
+         Author build()
+         {
+            return Author("Pen writer", domain::country::test_countries::USA);
+         }
+         
+      };
+
+      AuthorTestBuilder anAuthor()
+      {
+         return {};
+      }*/
+   }
+}
+
+#endif //TESTBUILDERS_WORKSHOP_TESTS_DOMAIN_BOOK_AUTHOR_TEST_BUILDER_H_INCLUDED
