9127ce755a757b4aed969fc4ef0694beb2646704
Philippe Bourgau <philippe.bourgau@murex.com>
2020-06-28 07:39:36 +0200

    Bug in Report Generator fixed with a test build with mikado and test data builder

--- src/main/cpp/lib/ReportGenerator.cpp
+++ src/main/cpp/lib/ReportGenerator.cpp
@@ -29,9 +29,9 @@ namespace report
 		double totalAmount = 0.0;
 		for (const auto id2Invoice : invoiceMap)
 		{
-         totalAmount += id2Invoice.second->computeTotalAmount();
-         //const auto& invoice = *id2Invoice.second;
-         //totalAmount += finance::toUSD(invoice.computeTotalAmount(), invoice.getCountry().getCurrency());
+         //totalAmount += id2Invoice.second->computeTotalAmount();
+         const auto& invoice = *id2Invoice.second;
+         totalAmount += finance::toUSD(invoice.computeTotalAmount(), invoice.getCountry().getCurrency());
       }
 		return totalAmount;
 	}
