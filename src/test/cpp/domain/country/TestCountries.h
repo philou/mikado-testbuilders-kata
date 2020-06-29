9442063813c37482214303072fbe508998dc1da8
Philippe Bourgau <philippe.bourgau@murex.com>
2020-06-29 10:24:19 +0200

    Prepare empty headers and directories to speed up testing

---
+++ src/test/cpp/domain/country/TestCountries.h
@@ -0,0 +1,15 @@
+#ifndef TESTBUILDERS_WORKSHOP_TESTS_DOMAIN_COUNTRY_TEST_COUNTRIES_H_INCLUDED
+#define TESTBUILDERS_WORKSHOP_TESTS_DOMAIN_COUNTRY_TEST_COUNTRIES_H_INCLUDED
+
+namespace domain
+{
+   namespace country
+   {
+      namespace test_countries
+      {
+         
+      }
+   }
+}
+
+#endif
